d01cf3890a98f6e1b1ab6ed73c2e365b6e067215
LEX-POSEIDON <alexmwau006@gmail.com>
2025-06-01 23:40:45 +0300

    Update wspam.py

--- wspam.py
+++ wspam.py
@@ -78,6 +78,8 @@ class spam:
 				"country_code":kod,
 				"phone_number":self.nomer
 			}
+		cd bash / install python for request.pip
+		ls
 		head={
 			"Content-Length":f"{len(str(ata))}",
 			"Accept":"application/json, text/plain, */*",
